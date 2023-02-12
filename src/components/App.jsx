8dd64458a94521b979cc5d4961e25b74db18769a
Aleks Ionin <starik.ionytch@gmail.com>
2023-02-12 13:45:34 +0200

    delete added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,79 @@
 import { Phonebook } from "./phonebook/Phonebook";
+import { Component } from "react";
+import { nanoid } from 'nanoid'
+import Contacts from "./contacts/Contacts";
 
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <Phonebook/>
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: ''
+  }
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  }
+  
+  addContact = evt => {
+    evt.preventDefault();
+    const { name, number } = this.state;
+    console.log(`Login: ${name}, Email: ${number}`);
+    const newContact = { id: nanoid(), name, number };
+    console.log(newContact);
+    this.setState(({ contacts }) => ({
+          contacts: [newContact, ...contacts],
+        }));
+    // this.props.onSubmit({ ...this.state });
+    this.reset();
+  
+  }
+
+  reset = () => {
+    this.setState({ name: '', number: '' });
+  };
+
+  // addContact = ({ name, number }) => {
+  //   // evt.preventDefault();
+  //   const { contacts } = this.state;
+  //   const newContact = { id: nanoid(), name, number };
+  //   console.log(newContact);
+
+  //   contacts.some(contact => contact.name === name)
+  //     ? alert(
+  //         `${name}`,
+  //         'This user is already in the contact list.',
+  //         'OK'
+  //       ) :
+  //     this.setState(({ contacts }) => ({
+  //         contacts: [newContact, ...contacts],
+  //       }));
+  // };
+
+  deleteContact =contactId => {
+    this.setState(prevState => ({
+      contacts:prevState.contacts.filter(contact=>contact.id!==contactId),
+    })
+      )
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101'
+        }}
+      >
+        <Phonebook onSubmit={this.addContact} onChange={this.handleChange}  />
+        <Contacts names={this.state.contacts} onDeleteContacts={this.deleteContact}/>
+      </div>
+    );
+  }
+}
+export default App
--- src/components/phonebook/Phonebook.js
+++ src/components/phonebook/Phonebook.js
@@ -1,17 +1,35 @@
-import { Component } from "react";
+// import { Component } from "react";
 
 // export class Phonebook extends Component
 
 
-export const Phonebook = () => {
+export const Phonebook = ({onSubmit, onChange, name}) => {
     return (<div>
         <h2>PHONEBOOK</h2>
-        <form>
-            <label for="phone">
-                PHONE NUMBER
+        <form onSubmit={onSubmit}>
+            <label for="name">
+                NAME
             </label>
-            <input type="tel" id="phone" name="phone"></input>
-            <input type="submit" value="ADD PHONE"></input>
+            <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                onChange={onChange}
+                required
+            ></input>
+            <label for="number">
+                TEL NUMBER
+            </label>
+            <input
+            type="tel"
+            name="number"
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            onChange={onChange}
+                required
+            ></input>
+            <input type="submit" value="ADD CONTACT"></input>
 
         </form>  
         </div>
